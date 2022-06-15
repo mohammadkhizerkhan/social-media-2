c401ec6db181fef732191eb0f4e9647a359558b2
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-06-16 00:56:47 +0530

    completed feed posts and user comment navigate

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -8,14 +8,17 @@ import {
   Button,
 } from "@chakra-ui/react";
 import {useSelector} from "react-redux"
+import { useNavigate } from "react-router-dom";
 
 function CommentCard({ comment }) {
   const { text, username } = comment;
   const {allUsers} = useSelector(store => store.user)
   const userDetails=allUsers.find(user=>user.username===username)
+  const navigate=useNavigate();
   return (
     <HStack alignItems="start">
       <Avatar
+      cursor="pointer" onClick={()=>navigate(`/user-profile/${userDetails.userId}`)}
         name="Ryan florence"
         size="sm"
         src="https://bit.ly/ryan-florence"
@@ -27,7 +30,7 @@ function CommentCard({ comment }) {
         borderRadius="5px"
         color={useColorModeValue("gray.600", "gray.300")}
       >
-        <Button variant="link" display="block" fontWeight="bold">{userDetails?.firstName} {userDetails?.lastName}</Button>
+        <Button variant="link" cursor="pointer" onClick={()=>navigate(`/user-profile/${userDetails.userId}`)} display="block" fontWeight="bold">{userDetails?.firstName} {userDetails?.lastName}</Button>
         {text}
       </Box>
     </HStack>
