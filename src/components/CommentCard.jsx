5395be4ee2b546d05a95f2db046cf985c7731ec3
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-06-20 11:53:26 +0530

    changed menu in comment

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -6,69 +6,70 @@ import {
   useColorModeValue,
   HStack,
   Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
   IconButton,
+  VStack,
 } from "@chakra-ui/react";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { MdOutlineMoreVert } from "react-icons/md";
 import { deleteComment } from "../features/post/PostSlice";
+import { EmailIcon } from "@chakra-ui/icons";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
-function CommentCard({ comment,postId }) {
-  const { text, username,_id } = comment;
+function CommentCard({ comment, postId }) {
+  const { text, username, _id } = comment;
   const { allUsers } = useSelector((store) => store.user);
   const userDetails = allUsers.find((user) => user.username === username);
   const { user } = useSelector((store) => store.auth);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [editComment,setEditComment]=useState({text:text})
+  const [editComment, setEditComment] = useState({ text: text });
   return (
-    <HStack alignItems="start" w="full">
-      <Avatar
-        cursor="pointer"
-        onClick={() => navigate(`/user-profile/${userDetails.userId}`)}
-        name="Ryan florence"
-        size="sm"
-        src="https://bit.ly/ryan-florence"
-      />
-      <Box
-        mt={2}
-        p={2}
-        bg={useColorModeValue("gray.200", "gray.600")}
-        borderRadius="5px"
-        color={useColorModeValue("gray.600", "gray.300")}
-        w="full"
-      >
-        <Button
-          variant="link"
+    <>
+      <HStack alignItems="start" w="full">
+        <Avatar
           cursor="pointer"
           onClick={() => navigate(`/user-profile/${userDetails.userId}`)}
-          display="block"
-          fontWeight="bold"
-        >
-          {userDetails?.firstName} {userDetails?.lastName}
-        </Button>
-        {text}
-      </Box>
-      <Menu placement="bottom-end">
-        <MenuButton
-          as={IconButton}
-          w="22px"
-          h="22px"
-          aria-label="Options"
-          icon={<MdOutlineMoreVert />}
-          variant="outline"
-          display={username === user.username ? "flex" : "none"}
+          name="Ryan florence"
+          size="sm"
+          src="https://bit.ly/ryan-florence"
         />
-        <MenuList>
-          <MenuItem >Edit</MenuItem>
-          <MenuItem onClick={()=>dispatch(deleteComment({postId:postId,commentId:_id}))}>Delete</MenuItem>
-        </MenuList>
-      </Menu>
-    </HStack>
+        <VStack
+          alignItems="flex-start"
+          mt={2}
+          p={2}
+          bg={useColorModeValue("gray.200", "gray.600")}
+          borderRadius="5px"
+          color={useColorModeValue("gray.600", "gray.300")}
+          w="full"
+        >
+          <Button
+            variant="link"
+            cursor="pointer"
+            onClick={() => navigate(`/user-profile/${userDetails.userId}`)}
+            display="block"
+            fontWeight="bold"
+          >
+            {userDetails?.firstName} {userDetails?.lastName}
+          </Button>
+          <span>{text}</span>
+          <button
+            style={{
+              margin: 0,
+              width: "2.5rem",
+              height: "2.5rem",
+              backgroundColor: "lightgrey",
+              display:"flex",
+              justifyContent:"center",
+              alignItems:"center",
+              color: "black",
+            }}
+          >
+            <IoCloseCircleOutline />
+          </button>
+        </VStack>
+      </HStack>
+    </>
   );
 }
 
